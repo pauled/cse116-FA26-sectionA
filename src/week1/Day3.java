722fd68bdf6f0adc597d14283079279a48ee475c
drpaul <drpauled@gmail.com>
2026-08-28 13:53:30 -0400

    day 1 of class

---
+++ src/week1/Day3.java
@@ -0,0 +1,21 @@
+package week1;
+
+public class Day3{
+    public static double multiplyByTwo(double valIn){
+        double temp=2*valIn;
+        return temp;
+    }
+    public static void main(String[] args) {
+        System.out.println("hello nurse");
+        int x=6;//int are integers and store integers
+        //these are single line comments
+        /*
+        this is how you do a multi
+        line comment
+        */
+       double y=3.2;//doubles are like floats
+       String str1="my string";//' can't be used for strings
+       boolean true1=false;
+       double out=multiplyByTwo(y);
+    }
+}
